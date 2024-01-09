5b27365672a07fa698314a634739c15090a4885b
shadowprince55 <139494758+shadowprince55@users.noreply.github.com>
2024-01-09 23:57:23 +0530

    Create 9.cpp

---
+++ GFG/2024/January/9.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+        vector <int> search(string pat, string txt)
+        {
+            //code here
+            vector<int>ans;
+            int n = txt.size();
+            int m = pat.size();
+            for(int i=0;i<(n-m+1);i++){
+                bool flag = 1;
+                int k=0;
+                for(int j=i;j<(i+m);j++){
+                    if(txt[j]==pat[k])
+                        k++;
+                    else{
+                        flag = 0;
+                        break;
+                    }
+                }
+                if(flag)
+                    ans.push_back(i+1);
+            }
+            return ans;
+            
+            
+        }
+     
+};
